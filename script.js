bd93e005721856b72b359d3ad56bc86112197c82
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2022-06-12 21:00:03 -0300

    Adicionada funcionalidade que permite utilizar as teclas do teclado para interagir com a calculadora

--- script.js
+++ script.js
@@ -31,10 +31,52 @@ Object.entries(containerButtonsFunc).forEach(([selector, func]) => {
     );
 });
 
-
 const btnTestCalculator = document.querySelector('.test-calculator');
 
 btnTestCalculator.addEventListener('click', () => {
-    testCalculator.runTests();
-})
+  testCalculator.runTests();
+});
 
+document.addEventListener('keyup', ({ key }) => {
+  const keyboardButtons = {
+    0: '#key0',
+    1: '#key1',
+    2: '#key2',
+    3: '#key3',
+    4: '#key4',
+    5: '#key5',
+    6: '#key6',
+    7: '#key7',
+    8: '#key8',
+    9: '#key9',
+    '/': '#divideOperator',
+    '*': '#timesOperator',
+    '-': '#minusOperator',
+    '+': '#plusOperator',
+    '.': '#decimal',
+    ',': '#decimal',
+    '=': '#equal',
+    Enter: '#equal',
+    Escape: '#clearCalc',
+    Backspace: '#backspace',
+  };
+
+  const selector = keyboardButtons[key];
+
+  if (!selector) return;
+
+  pressButton(selector);
+});
+
+function pressButton(selector) {
+  const element = document.querySelector(selector);
+  if(element){
+    var event = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+    
+    element.dispatchEvent(event);
+  }
+}
